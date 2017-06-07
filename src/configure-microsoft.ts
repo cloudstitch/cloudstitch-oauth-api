c74296e4fed8a388b4dbaa4c8580c315d970f09e
Ethan Apodaca <papodaca@gmail.com>
2017-06-07 13:31:12 -0700

    configure MS correctly

--- src/configure-microsoft.ts
+++ src/configure-microsoft.ts
@@ -12,6 +12,8 @@ export function Configure(router: any, passport: any) {
     let opts = {
       clientId: Constants[SERVICE].ClientID,
       clientSecret: Constants[SERVICE].ClientSecret,
+      tenantIs: Constants[SERVICE].TenantID,
+      resource: Constants[SERVICE].Resouce,
       callbackURL: Constants.callbackURLs[Constants.environmentName][SERVICE],
       passReqToCallback: true
     };
